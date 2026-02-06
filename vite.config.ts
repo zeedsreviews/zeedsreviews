4f121d2a273adffd061d55de65f39380428ecce7
zeedsreviews <shoyombomoshood1@gmail.com>
2026-02-06 20:27:53 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/zeedsreviews/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
