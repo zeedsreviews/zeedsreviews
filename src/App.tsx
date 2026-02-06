8911217c5711825919dc384eefbe46f489119018
zeedsreviews <shoyombomoshood1@gmail.com>
2026-02-06 20:35:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zeedsreviews">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/reviews" element={<Reviews />} />
