3177902f8ee6bbd72c7fab729c32cba9cf4b666a
zeedsreviews <shoyombomoshood1@gmail.com>
2026-02-06 21:26:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/zeedsreviews/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
